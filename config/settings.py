f2f752a629d3352ca3a0be30c4b9e7275355d3d4
1Kanan1 <83110215+1Kanan1@users.noreply.github.com>
2025-07-09 20:22:54 +0400

    chore: rename POSTGRES_DATABASE to POSTGRES_DB

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     "default": (
         {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": env("POSTGRES_DATABASE"),
+            "NAME": env("POSTGRES_DB"),
             "USER": env("POSTGRES_USER"),
             "PASSWORD": env("POSTGRES_PASSWORD"),
             "HOST": env("POSTGRES_HOST"),
